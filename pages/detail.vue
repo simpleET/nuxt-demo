2ecedcf74d992942cb10e47db580f6493cc8543b
lsk <longsk@casc.ac.cn>
2020-09-28 17:05:01 +0800

    添加别人的接口

--- pages/detail.vue
+++ pages/detail.vue
@@ -2,6 +2,7 @@
   <div class="detail-page">
     <h5>{{ip}}</h5>
     {{JSON.stringify(detail)}}
+    {{JSON.stringify(home_page)}}
     <ul>
     </ul>a
   </div>
@@ -19,11 +20,14 @@
     async asyncData({$axios}) {
       console.log(343443)
       // const detail = await $axios.$get('http://192.168.20.116:819/api/talentMap/creativePlatform?timestamp=1595839686961');
-      const detail = await $axios.$get('/talentMap/creativePlatform?timestamp=1595839686961');
-      const ip = await $axios.$get('http://icanhazip.com');
+      // const detail = await $axios.$get('/talentMap/creativePlatform?timestamp=1595839686961');
+      // const ip = await $axios.$get('http://icanhazip.com');
+      const home_page = await $axios.$get('https://www.devio.org/io/flutter_app/json/home_page.json');// 别人的接口
 
-      console.log(detail)
-      return {detail, ip}
+      console.log(home_page)
+      return {home_page}
+      // console.log(detail,home_page)
+      // return {detail, ip,home_page}
     },
   }
 </script>
