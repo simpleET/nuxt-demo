4dca165e060e4524ca134386ab6e6c61e56671c6
lsk <longsk@casc.ac.cn>
2020-09-28 16:36:21 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'universal',
+  // mode: 'spa', // 会禁用服务器端渲染
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  },
+  axios: {
+    proxy: true,
+    prefix: '/api/',
+    credentials: true,
+  },
+  proxy: {
+    '/api': {
+      target: 'http://192.168.20.116:819',
+      changeOrigin: true,
+    /*  pathRewrite: {
+        '^/api': ''
+      }*/
+    },
+  },
+}
---
+++ pages/detail.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="detail-page">
+    <h5>{{ip}}</h5>
+    {{JSON.stringify(detail)}}
+    <ul>
+    </ul>a
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        title: '详情页',
+      }
+    },
+    async asyncData({$axios}) {
+      console.log(343443)
+      // const detail = await $axios.$get('http://192.168.20.116:819/api/talentMap/creativePlatform?timestamp=1595839686961');
+      const detail = await $axios.$get('/talentMap/creativePlatform?timestamp=1595839686961');
+      const ip = await $axios.$get('http://icanhazip.com');
+
+      console.log(detail)
+      return {detail, ip}
+    },
+  }
+</script>
+<style scoped>
+  ul {
+    display: block;
+    border: 1px solid red;
+  }
+  li {
+    color: blue;
+  }
+
+</style>
